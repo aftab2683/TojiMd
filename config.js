8b6ddc47864b46fe6f8c41348212d7e37edc370b
aftab2683 <111147820+aftab2683@users.noreply.github.com>
2024-12-30 21:58:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "2347030229711"
+const ownervb = process.env.OWNERS || "923015326254"
 
 
 const ownerlist = ownervb.split(';');
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2347030229711', '254700143167']
-global.prems = ['254743706010', '254700143167', '254755267907']
-global.allowed = ['254743706010', '254700143167']
+global.mods = ['923015326254', '923015326254']
+global.prems = ['923015326254', '923015326254', '923015326254']
+global.allowed = ['923015326254', '923015326254']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -71,8 +71,8 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'silva'
-global.author = 'silva'
+global.packname = 'aftab'
+global.author = 'aftab'
 global.menuvid = ''
 global.igfg = ''
 global.dygp = ''
